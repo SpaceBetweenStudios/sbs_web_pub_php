1556cc7db2f764e98016cbbff308f8a3be88e3a9
Seg <seg@segonmedia.com>
2013-03-02 18:13:41 -0800

    Inital conversion

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html xmlns:fb='http://www.facebook.com/2008/fbml' xmlns:og='http://opengraphprotocol.org/schema/' xmlns='http://www.w3.org/1999/xhtml'>
+<head>
+  <title>Space Between Studios</title>
+  <link href='/favicon.ico' rel='shortcut icon' type='image/x-icon'>
+  <link href='http://www.spacebetweenstudios.com' rel='index' title='Space Between Studios'>
+  <link href='https://plus.google.com/105806783098119777725' rel='publisher'>
+  <meta content="summary" name="twitter:card" />
+  <meta content="121975404636485" property="fb:app_id" />
+  <meta content="Space Between Studios" property="og:title" />
+  <meta content="http://www.spacebetweenstudios.com/" property="og:url" />
+  <meta content="An independent game studio of non-fiction games." property="og:description" />
+  <meta content="website" property="og:type" />
+  <meta content="/img/sbs_logo_512.jpg" property="og:image" />
+  <meta content="Space Between Studios" property="og:site_name" />
+  <link href="/css/application.css" media="screen" rel="stylesheet" type="text/css" />
+  <script src="/js/application.js" type="text/javascript"></script>
+  <script src="/js/jquery.js" type="text/javascript"></script>
+  <script>
+  //<![CDATA[
+    (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=121975404636485";
+    fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+  //]]>
+</script>
+
+<script>
+  //<![CDATA[
+    var _gaq = _gaq || [];
+    var pluginUrl = '//www.google-analytics.com/plugins/ga/inpage_linkid.js';
+    _gaq.push(['_require', 'inpage_linkid', pluginUrl]);
+    _gaq.push(['_setAccount', 'UA-37064735-2']);
+    _gaq.push(['_setDomainName', 'spacebetweenstudios.com']);
+    _gaq.push(['_setAllowLinker', true]);
+    _gaq.push(['_trackPageview']);
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+  //]]>
+</script>
+<script>
+  //<![CDATA[
+    (function() {
+    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+    po.src = 'https://apis.google.com/js/plusone.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
+  //]]>
+</script>
+</head>
+<body>
+<div id='fb-root'></div>
+<div id='mainBody'>
+<img alt="Space Between Studios" height="512" src="/img/sbs_logo_512.jpg" width="512" />
+<p>
+An independent game studio of non-fiction games.
+</p>
+<p class='smaller'>
+More information soon. Follow these social networks below to stay informed.
+</p>
+<p>
+<a href="https://twitter.com/SpaceBtwStudios" class="twitter-follow-button" data-show-count="false" data-size="medium">Follow @SpaceBtwStudios</a>
+<span class='GooglePlus'>
+<span class='g-plusone' data-href='https://plus.google.com/105806783098119777725'></span>
+</span>
+<div class='fb-like' data-colorscheme='dark' data-href='http://www.facebook.com/SpaceBetweenStudios' data-send='false' data-show-faces='false' data-width='200'></div>
+</p>
+</div>
+<div id='footer'>
+&copy; 2013 -
+<script type="text/javascript">eval(decodeURIComponent('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%5c%22%6d%61%69%6c%74%6f%3a%69%6e%66%6f%40%73%70%61%63%65%62%65%74%77%65%65%6e%73%74%75%64%69%6f%73%2e%63%6f%6d%5c%22%3e%53%70%61%63%65%20%42%65%74%77%65%65%6e%20%53%74%75%64%69%6f%73%3c%5c%2f%61%3e%27%29%3b'))</script>
+</div>
+</body>
+</html>
